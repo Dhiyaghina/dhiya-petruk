033f372047f83d45c0534624153221620f317f1b
Dhiyaghina <112570933+Dhiyaghina@users.noreply.github.com>
2023-04-13 11:27:18 +0700

    baru

--- belajar c string.cpp
+++ belajar c string.cpp
@@ -1,15 +1,21 @@
-//#include <iostream>
-//#include <cstring>
-//using namespace std;
-//int main(){
-//	char nama[]="hamid";
-//	char prodi[]="ilkom";
-//	cout<<nama<<endl;
-//	cout<<strlen(nama)<<endl;//strlen agar karakter null tidak dibaca
-//	
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+	char nama[]="ghina";
+	char prodi[]="ilkom";
+	char npm[]="22170510";
+	cout<<nama<<endl;
+
+		//hitung jumlah variabelnya
+//	cout<<strlen(nama)<<endl; //strlen agar karakter null tidak dibaca
+//
 //	cout<<prodi<<endl;
-//	cout<<sizeof(prodi)<<endl;//sizeof karakter null dibaca
+//	
+//	cout<<sizeof(prodi)<<endl; //sizeof karakter null dibaca
 //	cout<<endl;
+		
+		//mengeluarkan huruf satu satu
 //	for (int i=0;i<strlen(nama);i++){
 //		cout<<nama[i]<<" "<<(nama[i]=='\0'?"Null":"Not Null")<<endl;
 //	}
@@ -17,16 +23,18 @@
 //	for (int i=0;i<sizeof(prodi);i++){
 //		cout<<prodi[i]<<" "<<(prodi[i]=='\0'?"Null":"Not Null")<<endl;
 //	}
-
+		
+		//masukin langsung per huruf
 //	char str[]={'i','l','k','o','m','\0'};
 //	cout<<str<<endl;
 //	cout<<strlen(str)<<endl;
-//	char nama[15];
+//	char nama[6];
 //	char npm[10];
-//	cin.get(nama,15);
+//	cin.get(nama,6);
 //	cin.ignore();
 //	cin.get(npm,10);
-//	
+
+		//ganti huruf a jadi x	
 //	cout<<"nama : " <<nama<<endl;
 //	cout<<"npm : "<<npm<<endl;
 //	char karakter;
@@ -37,24 +45,30 @@
 //				cout<<karakter;
 //			
 //		}
+//}
 //	
-//	char fname[50] = "imran";
-//	char lname[50]= " hamid";
-//	char *fullname = strcat(fname, lname);//strcat untuk menyatukan char atau string
-//	cout<<fullname;
+		//gabungin gname dan lname
+	char fname[50] = "dhiya";
+	char lname[50]= " ghina";
+	char *fullname = strcat(fname, lname);//strcat untuk menyatukan char atau string
+	cout<<fullname;
 
+		//mengubah variabel str1 menjadi long
 //	char* endptr;
-//	char str1[] = "011hamid";
+//	char str1[] = "011ghina";
 //	long value1 = strtol(str1, &endptr, 2);//mengubag variabel str1 menhjadi long
 //	cout<<value1<<endl;
 //	cout<<endptr<<endl;//endptr untuk memisahkan angka dengan huruf
-//	
-//	char str2[] = "110hamid";
+//}
+
+		//mengubah str2 menjadi unsigned long
+//	char str2[] = "110ghina";
 //	unsigned long value2 = strtoul(str2, &endptr, 8);//mengubah str2 menjadi unsigned long
 //	cout<<value2<<endl;
 //	cout<<endptr<<endl;
-//	
-//	char str3[] = "1hamid";
+
+		// mengubah str3 menjadi double
+//	char str3[] = "1ghina";
 //	double value3 = strtod(str3, &endptr);// mengubah str3 menjadi double
 //	cout<<value3<<endl;
 //	cout<<endptr<<endl;
